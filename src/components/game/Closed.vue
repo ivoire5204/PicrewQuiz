f9c2b3d7afc3613d49ab3e193b7bc382e0f949f3
ivoire5204 <49222346+ivoire5204@users.noreply.github.com>
2021-07-19 17:07:10 +0900

    게임 페이지 수정

---
+++ src/components/game/Closed.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <header>
+            <h1>{{ username }} 님의 게임</h1>
+            <p>{{ timecreated }} 생성됨</p>
+        </header>
+        <p id="status">이미 종료된 게임이에요!</p>
+        <div id="gallery">
+            <v-btn id="leftarrow" elevation="0" fab>&#60;</v-btn>
+            <div class="gamecontent" v-bind="gamedata">
+                <!--v-for 사용-->
+                <v-img src=""></v-img>
+                <!--유저 이름 태그-->
+            </div>
+            <v-btn id="rightarrow" elevation="0" fab>&#62;</v-btn>
+        </div>
+        <div id="appliers" v-bind:key="appliers">
+            <p>그림을 제출해주신 분</p>
+            <li>
+                <ul>{{ username }}</ul> 
+            </li>
+        </div>
+        <div id="participants" v-bind="participants">
+            <p>퀴즈에 참여해주신 분</p>
+            <li>
+                <ul>{{ username }}</ul> <!--v=for-->
+            </li>
+        </div>
+        <p>{{ username }}님이 가장 많이 맞췄어요!</p>
+        <p>{{ username }}님이 가장 많은 연합우주원을 속였어요!</p>
+        <p>{{ username }}님이 가장 많은 연합우주원들에게 들켰어요!</p>
+    </div>
+</template>
---
+++ src/components/game/Opened.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header>
+            <h1>{{ username }} 님의 게임</h1>
+            <p>{{ timecreated }} 생성됨</p>
+        </header>
+        <p id="status">게임이 진행되고 있어요!</p>
+        <p>참가 신청 시간이 {{ timeleft }} 남았어요!</p>
+        <p><a href="picrewlink">이 링크</a>의 픽크루 템플릿을 통해 당신의 개성을 표현해주세요!</p>
+        <div id="thumbnail">
+            <v-img src="thumbnailsrc"></v-img>
+        </div>
+        <v-form>
+            <v-row>
+                신청자님의 개성이 담긴 그림을 업로드해주세요!
+            </v-row>
+            <!--파일 드래그앤드롭-->
+        </v-form>
+    </div>
+</template>
