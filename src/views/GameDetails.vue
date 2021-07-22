fd79ca108ba420ffbb75f38b4280e4b7aade8620
ivoire5204 <49222346+ivoire5204@users.noreply.github.com>
2021-07-22 19:52:21 +0900

    GameDetails 일부 수정

--- src/views/GameDetails.vue
+++ src/views/GameDetails.vue
@@ -5,10 +5,25 @@
             <p>{{ timecreated }} 생성됨</p>
         </header>
         <p>{{ statusmsg }}</p>
-        <p><a href="picrewlink">이 링크</a>의 픽크루 템플릿을 통해 당신의 개성을 표현해주세요!</p>
-        <a href="picrewlink">
+        <p><a v-bind:href="picrewlink">이 링크</a>의 픽크루 템플릿을 통해 당신의 개성을 표현해주세요!</p>
+        <a v-bind:href="picrewlink">
             <v-img id="thumbnail" v-bind:src="thumbnailsrc"></v-img>
         </a>
+        <v-form>
+            <v-row>
+                당신의 개성이 담긴 그림을 업로드해주세요!
+            </v-row>
+            <v-row>
+                <!--드래그 앤 드롭 방식 적용 예정-->
+                <v-file-input></v-file-input>
+            </v-row>
+            <v-row>
+                <v-btn>제출하기</v-btn>
+            </v-row>
+        </v-form>
+        <!--게임 제안자에게 보이는 메시지-->
+        <p>신청을 미리 끝내고 게임을 진행할까요?</p>
+        <v-btn>미리 끝내기</v-btn>
     </div>
 </template>
 
@@ -35,6 +50,9 @@ export default {
 <style>
     #thumbnail {
         width: 300px;
-        margin: 0px auto;
+        margin: 20px auto;
+    }
+    .v-file-input {
+        width: 50px;
     }
 </style>
